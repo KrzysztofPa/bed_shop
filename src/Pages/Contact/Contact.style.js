0c3598cf927dc27b86a229a10cd341b4b3c396a1
Krzysztof Pawlak <krzysztof.pawlak@netteam.pl>
2022-04-20 07:23:02 +0200

    add contact page

---
+++ src/Pages/Contact/Contact.style.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+export const ContactForm = styled.form`
+  margin-top: 4rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`
+
+export const SendMassage = styled.p`
+  color: green;
+  font-weight: bold;
+`
+
+export const ContactInput = styled.input`
+  min-width: 14rem;
+`
+
+export const ContactTextArea = styled.textarea`
+  min-width: 14rem;
+`
--- src/Pages/Contact/Contact.tsx
+++ src/Pages/Contact/Contact.tsx
@@ -1,5 +1,36 @@
 import {Content} from "../Global.style";
+import {ContactForm, ContactInput, ContactTextArea, SendMassage} from "./Contact.style";
+import {useState} from "react";
+import {Baner} from "../Main/Main.style";
 
 export const Contact = (): JSX.Element => {
-    return <Content>Contact</Content>
+
+    const [sendMassage, setSendMassage] = useState('')
+
+    const setMassage = (e: MouseEvent) => {
+        e.preventDefault();
+        setSendMassage('Wiadomośc została wysłana')
+    }
+
+    return <Content>
+        <Baner>
+            <ContactForm>
+                <label htmlFor='email'>
+                    Mail:
+                </label>
+                <ContactInput type='email' id='email'/>
+                <label htmlFor='massage'>
+                    Wiadomość:
+                </label>
+                <ContactTextArea id='massage'/>
+
+                <button onClick={(e) => setMassage(e as unknown as MouseEvent)}>Wyślij</button>
+
+                <SendMassage>
+                    {sendMassage}
+                </SendMassage>
+            </ContactForm>
+        </Baner>
+
+    </Content>
 }
